c5dcb7c49e65add8102547456fcc44ab4fcb3219
Kyle Brumm <kjbrum@msn.com>
2015-07-03 01:28:02 -0500

    Restructure shortcode and add filters

--- includes/fivehundred-shortcodes.php
+++ includes/fivehundred-shortcodes.php
@@ -21,31 +21,29 @@ class FiveHundred_Shortcodes {
      */
     function display_feed( $atts, $content ) {
 
-        if($atts['search']) {
+        if( !empty( $atts['search'] ) ) {
             $atts['term'] = $atts['search'];
-            unset($atts['search']);
+            unset( $atts['search'] );
         }
 
-        if($atts['count']) {
+        if( !empty( $atts['count'] ) ) {
             $atts['rpp'] = $atts['count'];
-            unset($atts['count']);
+            unset( $atts['count'] );
         }
 
-        if($atts['categories']) {
+        if( !empty( $atts['categories'] ) ) {
             $atts['only'] = $atts['categories'];
-            unset($atts['categories']);
+            unset( $atts['categories'] );
         }
 
-        if($atts['exclude_categories']) {
+        if( !empty( $atts['exclude_categories'] ) ) {
             $atts['exclude'] = $atts['exclude_categories'];
-            unset($atts['exclude_categories']);
+            unset( $atts['exclude_categories'] );
         }
 
-        if($atts['username']) {
+        if( !empty( $atts['username'] ) ) {
             $atts['feature'] = 'user';
         }
-
-        // Check for global options
 
         // Extract the attributes
         $atts = shortcode_atts( array(
@@ -61,19 +59,24 @@ class FiveHundred_Shortcodes {
             'image_size'     => '3',
             'include_store'  => '0',
             'include_states' => '0',
-            'tags'           => '1'
+            'tags'           => '1',
+            'heading'        => ''
         ), $atts );
 
         $photos = $this->get_photos( array_filter( $atts ) );
 
-        $feed = '';
-        $feed .= "<h4>500px Connector Feed</h4>";
+        $feed = "<div class='fivehundred-container'>
+                    <ul class='fivehundred-items'>";
+                        foreach($photos as $photo) {
+                            $feed .= "<li class='fivehundred-item'>";
+                                        $item_info = "<a href='http://500px.com/{$photo['url']}' target='_blank'>{$photo['name']}<br><img src='{$photo['image_url']}'></a>";
+                                        $feed .= apply_filters( 'fivehundred_shortcode_item_contents', $item_info, $photo );
+                                    $feed .= '</li>';
+                        }
+            $feed .= '</ul>';
+        $feed .= '</div>';
 
-        foreach($photos as $photo) {
-            $feed .= "<div style='width: 50%; float: left; margin-bottom: 2rem;'><a href='http://500px.com/{$photo['url']}' target='_blank'>{$photo['name']}<br><img src='{$photo['image_url']}'></a></div>";
-        }
-
-        return $feed;
+        return apply_filters( 'fivehundred_shortcode_contents', $feed, $photos );
     }
 
     function get_photos( $data ) {
@@ -92,7 +95,7 @@ class FiveHundred_Shortcodes {
         if( $data['photos'] ) {
             return $data['photos'];
         } else {
-            return 'No photos meet your criteria.';
+            return apply_filters( 'fivehundred_shortcode_no_results', 'No photos meet your criteria.' );
         }
     }
 }
--- readme.md
+++ readme.md
@@ -6,6 +6,14 @@
 ### Shortcode
 
 
+### Filters/Actions
+
+`fivehundred_shortcode_item_contents`
+
+`fivehundred_shortcode_contents`
+
+`fivehundred_shortcode_no_results`
+
 ### To-Do
 
 - Let users choose from default display formats or add a way for them to make their own
@@ -13,6 +21,7 @@
    - Extra options for what info to return?
 - Create a shortcode (with options)
    - Extra options for what info to return?
+- Add in some filters and actions to make it more extensible
 - Allow the user to set default options for widgets and shortcodes
    - Exclude certain categories ("Nude" & per photo "nsfw": true)
 - Make a list of what options can be used, and what can be used together (i.e. feature="user", username="kjbrum")
