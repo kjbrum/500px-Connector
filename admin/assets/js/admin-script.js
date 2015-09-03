44c25797817899641a7f21d4fa5090820c2537c3
Kyle Brumm <kjbrum@msn.com>
2015-09-03 14:36:10 -0500

    Hide custom layout textarea if other option is selected

--- admin/assets/js/admin-script.js
+++ admin/assets/js/admin-script.js
@@ -2,8 +2,24 @@
  *  JavaScript for the admin area
  */
 
-// $ = jQuery.noConflict();
+$ = jQuery.noConflict();
 
-// (function($) {
-//     console.log('admin js!');
-// })($);
+(function($) {
+    var $layout = $('#default_layout');
+
+    if($layout.val() == 'custom') {
+        $('.custom-layout').show();
+    }
+
+    $layout.on('change', function() {
+        var new_layout = $(this).val();
+
+        if(new_layout == 'custom') {
+            $('.custom-layout').show();
+        } else {
+            $('.custom-layout').hide();
+        }
+    });
+
+    console.log('admin js!');
+})($);
